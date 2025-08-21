22727c7d378df7e3176ac60b78e1fc974b9c33aa
Bella Natasya <bellanatasyaam@gmail.com>
2025-08-21 10:16:38 +0700

    ulang faCility n faCility controller

---
+++ app/Models/Facility.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facility extends Model
+{
+    use HasFactory;
+
+    // Tentukan kolom yang boleh diisi massal
+    protected $fillable = [
+        'name',
+        'type',
+        'description',
+        'cost',
+        'biling_type'
+    ];
+}
