171c4dd2868b78b0c73603cc85291f7f27b9c4a8
Bella Natasya <bellanatasyaam@gmail.com>
2025-08-26 15:00:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\FacilityController;
 use App\Http\Controllers\TenantController;
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\ContractController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PropertyUnitController;
+use App\Http\Controllers\PropertyUnitFacilityController;
+use App\Http\Controllers\FacilityUsageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +30,19 @@ Route::Resource('companies', CompanyController::class);
 
 Route::resource('facilities', FacilityController::class);
 
+Route::get('/facilities/reset', [FacilityController::class, 'resetFacilities'])->name('facilities.reset');
+
 Route::resource('tenants', TenantController::class);
 
 Route::resource('properties', PropertyController::class);
+
+Route::resource('property_units', PropertyUnitController::class);
+// Route::delete('property_units/{id}', [PropertyUnitController::class, 'destroy'])->name('property_units.destroy');
+
+Route::resource('contracts', ContractController::class);
+
+Route::resource('payments', PaymentController::class);
+
+Route::resource('property_unit_facilities', PropertyUnitFacilityController::class);
+
+Route::resource('facility_usages', FacilityUsageController::class);
