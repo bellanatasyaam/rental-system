036c6a8516bedb11ea8e72a387569925a3ec958a
Bella Natasya <bellanatasyaam@gmail.com>
2025-08-28 16:13:20 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\FacilityController;
-use App\Http\Controllers\TenantController;
+use App\Http\Controllers\TenantController; 
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\ContractController;
 use App\Http\Controllers\PaymentController;
@@ -32,14 +32,22 @@ Route::resource('facilities', FacilityController::class);
 
 Route::get('/facilities/reset', [FacilityController::class, 'resetFacilities'])->name('facilities.reset');
 
+// === TENANTS ===
+Route::get('/tenants/print', [TenantController::class, 'print'])->name('tenants.print');
+Route::get('/tenants/print/{id}', [TenantController::class, 'printOne'])->name('tenants.print.one');
 Route::resource('tenants', TenantController::class);
-Route::put('/tenants/{tenant}', [TenantController::class, 'update'])->name('tenants.update');
 
 Route::resource('properties', PropertyController::class);
 
+// === PROPERTY UNITS ===
+Route::get('/property_units/manage', [PropertyUnitController::class, 'manage'])->name('property_units.manage');
+Route::post('/property_units/{id}/book', [App\Http\Controllers\PropertyUnitController::class, 'book'])->name('property_units.book');
 Route::resource('property_units', PropertyUnitController::class);
 // Route::delete('property_units/{id}', [PropertyUnitController::class, 'destroy'])->name('property_units.destroy');
 
+// === CONTRACTS ===
+Route::get('/contracts/print', [ContractController::class, 'print'])->name('contracts.print');  
+Route::get('/contracts/print/{id}', [ContractController::class, 'printOne'])->name('contracts.print.one');
 Route::resource('contracts', ContractController::class);
 
 Route::resource('payments', PaymentController::class);
