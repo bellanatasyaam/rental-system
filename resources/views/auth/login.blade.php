1138932788382c9c534c9752ff88877243a7dfed
Bella Natasya <bellanatasyaam@gmail.com>
2025-09-12 09:01:09 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,6 @@
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -44,4 +43,18 @@
             </x-primary-button>
         </div>
     </form>
+
+    <!-- Tombol Login dengan Google -->
+    <div class="mt-6">
+        <a href="{{ route('google.login') }}"
+           class="w-full flex items-center justify-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md">
+            <svg class="w-5 h-5 mr-2" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M21.35 11.1h-9.18v2.92h5.38c-.23 1.24-.92 2.29-1.95 2.99v2.5h3.16c1.86-1.71 2.93-4.23 2.93-7.2 0-.62-.06-1.23-.17-1.81z" />
+              <path d="M12.17 22c2.63 0 4.84-.87 6.45-2.35l-3.16-2.5c-.88.59-2.01.93-3.29.93-2.53 0-4.68-1.71-5.45-4h-3.25v2.52C5.13 19.7 8.38 22 12.17 22z" />
+              <path d="M6.72 13.58c-.2-.59-.32-1.22-.32-1.88s.12-1.29.32-1.88V7.3H3.47A9.82 9.82 0 0 0 2.35 11.7c0 1.6.38 3.11 1.12 4.4l3.25-2.52z" />
+              <path d="M12.17 4.78c1.43 0 2.72.49 3.73 1.45l2.8-2.8C16.99 1.7 14.79.78 12.17.78 8.38.78 5.13 3.08 3.47 6.37l3.25 2.52c.77-2.29 2.92-4.1 5.45-4.1z" />
+            </svg>
+            {{ __('Login dengan Google') }}
+        </a>
+    </div>
 </x-guest-layout>
