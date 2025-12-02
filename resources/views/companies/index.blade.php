bd6317fbbf57689cd21bbaec5acba6dce67dac02
Bella Natasya <bellanatasyaam@gmail.com>
2025-12-02 17:16:45 +0700

    index companies

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -46,10 +46,6 @@
                     <div class="flex items-center border rounded-md px-3 py-2 bg-white w-64">
                         <input type="text" placeholder="Search..." class="w-full border-none focus:ring-0 text-sm">
                     </div>
-
-                    <button class="px-3 py-2 border border-gray-300 rounded-md text-sm bg-white hover:bg-gray-100">
-                        Export CSV
-                    </button>
                 </div>
             </div>
 
